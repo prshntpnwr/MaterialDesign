efe17cc8b592892241b9731e0a58c26d4b480502
Prashant Panwar <prashant.panwar777@gmail.com>
2018-03-15 01:17:31 +0530

    refactor: add dialog activity and shared transition naimation between main activity and dialog activity

---
+++ app/src/main/java/com/example/prashant/materialdesign/DialogActivity.java
@@ -0,0 +1,27 @@
+package com.example.prashant.materialdesign;
+
+import android.os.Build;
+import android.support.annotation.RequiresApi;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class DialogActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dialog);
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    public void dismiss(View view) {
+        finishAfterTransition();
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    @Override
+    public void onBackPressed() {
+        finishAfterTransition();
+    }
+}
--- app/src/main/java/com/example/prashant/materialdesign/MainActivity.java
+++ app/src/main/java/com/example/prashant/materialdesign/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.prashant.materialdesign;
 
+import android.app.ActivityOptions;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
@@ -18,12 +21,19 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        final FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+                Intent intent = new Intent(MainActivity.this, DialogActivity.class);
+                ActivityOptions options = null;
+                if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.LOLLIPOP) {
+                    options = ActivityOptions.makeSceneTransitionAnimation(MainActivity.this, fab,
+                            getString(R.string.transition_dialog));
+                    startActivity(intent, options.toBundle());
+                }
             }
         });
     }
