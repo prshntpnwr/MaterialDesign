e2552f92cfe1bdb7247cf2e103c0980cad02a34b
Prashant Panwar <prashant.panwar777@gmail.com>
2018-03-17 22:51:38 +0530

    fix: fab dialog transition animation by adding dialog background

--- app/src/main/java/com/example/prashant/materialdesign/ui/DialogActivity.java
+++ app/src/main/java/com/example/prashant/materialdesign/ui/DialogActivity.java
@@ -1,12 +1,11 @@
 package com.example.prashant.materialdesign.ui;
 
-import android.os.Build;
-import android.support.annotation.RequiresApi;
+import android.os.Bundle;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.view.View;
 import android.widget.FrameLayout;
+import android.widget.LinearLayout;
 
 import com.example.prashant.materialdesign.R;
 import com.example.prashant.materialdesign.ui.transitions.FabTransform;
@@ -19,7 +18,7 @@ public class DialogActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dialog);
 
-        FrameLayout container = findViewById(R.id.container);
+        LinearLayout container = findViewById(R.id.container);
 
         if (!FabTransform.setup(this, container)) {
             MorphTransform.setup(this, container,
