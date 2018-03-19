ad67fc538891fd51a7e77972a4233fd579361d8d
Prashant Panwar <prashant.panwar777@gmail.com>
2018-03-19 23:49:54 +0530

    refactor: add layout animation in main activity recyclerview

--- app/src/main/java/com/example/prashant/materialdesign/ui/DetailActivity.java
+++ app/src/main/java/com/example/prashant/materialdesign/ui/DetailActivity.java
@@ -2,6 +2,7 @@ package com.example.prashant.materialdesign.ui;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 import com.example.prashant.materialdesign.R;
 
@@ -11,5 +12,9 @@ public class DetailActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
+    }
+
+    public void dismiss(View view) {
+        finishAfterTransition();
     }
 }
